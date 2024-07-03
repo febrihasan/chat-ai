03b994d4602ef708523d7bebed50d235b018a341
febrihasan <febri.hasan@akarinti.tech>
2024-07-03 09:10:41 +0700

    feat(chat-ai): new sample project

---
+++ src/main/java/tech/febrihasan/chatai/ChatAiApplication.java
@@ -0,0 +1,13 @@
+package tech.febrihasan.chatai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAiApplication.class, args);
+	}
+
+}
